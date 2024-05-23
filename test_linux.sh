34a0330b78f1d9df7e4fc7930a738c2ee2228750
Blackneeed <kontododatkowe.marcelclay@gmail.com>
2024-05-23 21:06:04 +0200

    Oops

--- test_linux.sh
+++ test_linux.sh
@@ -2,14 +2,14 @@
 
 # the rest of the args (after $1 (what to do) and $2 (source dir)) are cli args
 
-if [ $1 == "install" ]; then
+if [ "$1" = "install" ]; then
     echo Installing test pkg.
     echo The source of test pkg is located in $2 btw.
 fi
-if [ $1 == "remove" ]; then
+if [ "$1" = "remove" ]; then
     echo Removing test pkg.
     echo It\'s source is located at $2 btw.
 fi
-if [ $1 == "run" ]; then
+if [ "$1" = "run" ]; then
     echo Test. The source of this pkg is located at $2 btw.
 fi
